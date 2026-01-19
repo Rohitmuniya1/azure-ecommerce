a91fe2d66e6e48d3b13a1710b298af957b19ab5f
Rohitmuniya1 <rohitmuniya9526@gmail.com>
2026-01-19 18:07:58 +0530

    add addToCart functionality

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
 console.log("E-commerce site loaded");
+
+function addToCart(name, price) {
+  let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+  cart.push({ name, price });
+
+  localStorage.setItem("cart", JSON.stringify(cart));
+
+  alert(name + " added to cart");
+}
